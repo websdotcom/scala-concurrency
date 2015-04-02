06ccd8a2035a7e7446951f27c59f395d62f300d7
Thomas Gideon <thomas@webs.com>
2015-04-02 16:42:37 -0400

    Progress.

--- src/main/scala/org/learningconcurrency/ch1/Exercises.scala
+++ src/main/scala/org/learningconcurrency/ch1/Exercises.scala
@@ -4,7 +4,7 @@
 object Exercises extends App {
   // Write a function to add one to its arguments
   def plusOne(x: Int) = {
-    x
+    x + 1
   }
 
   println(plusOne(1) == 2)
@@ -12,7 +12,7 @@ object Exercises extends App {
 
   // Write a function that returns true if the number passed in is odd
   def isOdd(x: Int) = {
-    false
+    x % 2 == 1
   }
 
   println(isOdd(13) == true)
@@ -20,7 +20,7 @@ object Exercises extends App {
 
   // Write a function that takes a list of numbers and determines if any of them is odd
   def anyOdd(xs: List[Int]) = {
-    false
+    (false /: xs)(_ || isOdd(_))
   }
 
   println(anyOdd(List(2, 4, 6)) == false)
@@ -28,25 +28,28 @@ object Exercises extends App {
 
   // Write a more general function to detect if all items in a sequence meet a certain predicate
   def all[T](xs: Seq[T])(pred: T => Boolean): Boolean = {
-    false
+    (true /: xs)(_ && pred(_))
   }
   println(all(0 until 10)(isOdd) == false)
-  println(all(0 until 10 by 2)(isOdd) == true)
+  println(all(1 until 11 by 2)(isOdd) == true)
 
   // Things get trickier now...
 
   // Write a method to compose two functions
   // compose(g, f) should return a function that is the same as g(f(x))
 
-  def compose[A, B, C](g: B => C, f: A => B): A => C = ???
+  def compose[A, B, C](g: B => C, f: A => B): A => C = {
+    a => g(f(a))
+  }
   println(compose(isOdd, plusOne)(2) == true)
   println(compose(isOdd, plusOne)(3) == false)
   // Implement a fuse method with the  signature below.
   // If both of the arguments are a value, it returns both of them as a tuple.
   // Otherwise it returns Nothing
   // Use for-comprehensions.
-  def fuse[A, B](a: Option[A], b: Option[B]): Option[(A, B)] = ???
+  //def fuse[A, B](a: Option[A], b: Option[B]): Option[(A, B)] = {
+  //}
 
-  println(fuse(Some(2), None) == None)
-  println(fuse(Some(42), Some("Hello world!")) == Some(Tuple2(42, "Hello world!")))
+  //println(fuse(Some(2), None) == None)
+  //println(fuse(Some(42), Some("Hello world!")) == Some(Tuple2(42, "Hello world!")))
 }
